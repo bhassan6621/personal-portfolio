42fb365708d1adf81fed63e28ddfaa706e878545
Bibi Hassan <bibihassan@Bibis-MBP.lan>
2021-01-07 15:20:48 -0500

    removed about

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '.Header/header.js';
-import AboutMe from '.AboutMe/aboutMe.js';
 import Skills from '.Skills/skills.js';
 import Projects from '.Projects/projects.js';
 import Contact from '.Contact/contact.js';
@@ -17,7 +16,6 @@ class App extends Component {
       <div className="App">
         <h1>hi</h1>
         <Header />
-        <AboutMe />
         <Skills />
         <Projects />
         <Contact />
