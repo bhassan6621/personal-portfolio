dace3ad335000ad9f96199bdb5009e87c5e0d9de
Bibi Hassan <bibihassan@Bibis-MBP.lan>
2021-01-07 14:08:18 -0500

    added react component skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from '.Header/header.js';
+import AboutMe from '.AboutMe/aboutMe.js';
+import Skills from '.Skills/skills.js';
+import Projects from '.Projects/projects.js';
+import Contact from '.Contact/contact.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    );
+  }
 }
-
 export default App;
