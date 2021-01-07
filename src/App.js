e58cde2004cdd07d33db323c17109d013b495896
Bibi Hassan <bibihassan@Bibis-MBP.lan>
2021-01-07 15:05:27 -0500

    Revert "merge changes"
    This reverts commit bd0a39c01c79f593017e2c35a74d79acbc3d2aea.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+<<<<<<< HEAD
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+=======
 import React, { Component } from 'react';
 import './App.css';
 import Header from '.Header/header.js';
@@ -24,5 +50,5 @@ class App extends Component {
     );
   }
 }
-
+>>>>>>> d5f4470d1b611a0b600e5c0c75cec40524d56d32
 export default App;
