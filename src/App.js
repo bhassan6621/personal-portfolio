00b47cf736862e8f9b49c8ddcd622851f70ffa9a
Frank Palma <frankpalma12@gmail.com>
2021-01-25 13:00:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './Header/header.js';
-import AboutMe from './AboutMe/aboutMe.js';
-import Skills from './Skills/skills.js';
-import Projects from './Projects/projects.js';
-import Contact from './Contact/contact.js';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header/header.js";
+import AboutMe from "./AboutMe/aboutMe.js";
+import Skills from "./Skills/skills.js";
+import Projects from "./Projects/projects.js";
+import Contact from "./Contact/contact.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   render() {
     return (
       <div className="App">
-          <Header /> 
-          <AboutMe /> 
-          <Skills /> 
-          <Projects /> 
-          <Contact /> 
+        <Header />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
       </div>
-
     );
   }
 }
--- src/Projects/projects.js
+++ src/Projects/projects.js
@@ -1,13 +1,68 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { FaGithub } from "react-icons/fa";
 class Projects extends Component {
-    render() {
-        return (
-            <div className="projects">
-                <h1> projects </h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="projects">
+        <h1> projects </h1>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/180" />
+          <Card.Body>
+            <Card.Title>React JS Website</Card.Title>
+            <Card.Text>
+              Learn how you can build you own personal react js website!
+            </Card.Text>
+            <Button variant="">
+              <FaGithub />{" "}
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/180" />
+          <Card.Body>
+            <Card.Title>MyAutoDiff</Card.Title>
+            <Card.Text>
+              Build a compact library that helps you computer high order
+              derivaties in python.
+            </Card.Text>
+            <Button variant="">
+              <FaGithub />{" "}
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/180" />
+          <Card.Body>
+            <Card.Title>Tweetie</Card.Title>
+            <Card.Text>
+              Open source Messenger App that encrypts all your messages locally
+              in your device.
+            </Card.Text>
+            <Button variant="">
+              <FaGithub />{" "}
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://picsum.photos/180/180" />
+          <Card.Body>
+            <Card.Title>Space Invaders</Card.Title>
+            <Card.Text>
+              Javascript implementation of the classic game, Space Invaders.
+            </Card.Text>
+            <Button variant="">
+              <FaGithub />{" "}
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default Projects;
